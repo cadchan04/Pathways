0ff356c11982cc281aa35a28594e1617176fcfb1
Cadence Chan <112191194+cadchan04@users.noreply.github.com>
2026-02-05 21:45:30 -0500

    created react app, set basic home page and routes

---
+++ client/README.md
@@ -0,0 +1,12 @@
+# Client (React)
+
+## Tech Stack
+- React (Vite)
+- React Router
+- Axios
+
+## Setup
+```bash
+cd client
+npm install
+npm run dev
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+        <h1>Pathways</h1>
+        <p>Let's Get Going! 🌍</p>
+    </div>
+  )
+}
