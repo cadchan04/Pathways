ba24a8616e9a39c69ca4949b96dfeb82ea63d078
karaorander <korande@purdue.edu>
2026-02-23 16:36:56 -0500

    Added logout to accounts page

--- client/src/components/Account/Account.jsx
+++ client/src/components/Account/Account.jsx
@@ -1,8 +1,34 @@
+import { useAuth0 } from '@auth0/auth0-react';
+
 function Account() {
+  const { logout } = useAuth0();
   return (
     <div>
         <h2>Account</h2>
         <p>You can change your account settings here. ⚙️</p>
+
+        <button onClick={() =>
+          logout({
+            logoutParams: { returnTo: window.location.origin }
+          })
+        }
+        style={{
+          position: "absolute",
+          bottom: "20px",
+          left: "50%",
+          transform: "translateX(-50%)",
+          width: "200px",
+          padding: "12px",
+          backgroundColor: "#348ea8", /* navbar color */
+          color: "white",
+          border: "none",
+          borderRadius: "6px",
+          fontSize: "16px",
+          cursor: "pointer"
+        }}
+      >
+        Log Out
+      </button>
     </div>
   )
 }
